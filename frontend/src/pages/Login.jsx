d858e04ce5a2f9e5dc076f2ce06c9f41f9faf3f5
Manik Mistry <mkmistryhd2019@gmail.com>
2024-05-24 17:03:32 +0530

    minor changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -32,11 +32,11 @@ const Login = () => {
     <>
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
     <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Maxlence <span className=" text-red-300">Blog</span></Link></h1>
-    <h3><Link to="/register">Register</Link></h3>
+    <h3><Link to="/register">REGISTER</Link></h3>
     </div>
 <div className="w-full flex justify-center items-center h-[80vh] ">
        <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
-         <h1 className="text-xl font-bold text-left">Log in to your account</h1>
+         <h1 className="text-xl font-bold text-left">LOG IN</h1>
          <input onChange={(e)=>setEmail(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your email" />
          <input onChange={(e)=>setPassword(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="password" placeholder="Enter your password" />
          <button onClick={handleLogin} className="w-full px-4 py-4 text-lg font-bold text-white bg-red-300 hover:bg-red-400 rounded-lg ">Log in</button>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -37,11 +37,11 @@ const Register = () => {
     <>
       <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
       <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Maxlence <span className=" text-red-300">Blog</span></Link></h1>
-    <h3><Link to="/login">Login</Link></h3>
+    <h3><Link to="/login">LOGIN</Link></h3>
     </div>
     <div className="w-full flex justify-center items-center h-[80vh] ">
        <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
-         <h1 className="text-xl font-bold text-left">Create an account</h1>
+         <h1 className="text-xl font-bold text-left">CREATE AN ACCOUNT</h1>
          <input onChange={(e)=>setUsername(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your username" />
          <input onChange={(e)=>setEmail(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your email" />
          <input onChange={(e)=>setPassword(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="password" placeholder="Enter your password" />
